0c43525bb04da900697840f6004fc4c31a72d2a0
lingp <carl.lin@huolala.cn>
2020-11-24 17:18:03 +0800

    初始化项目

---
+++ .postcssrc.js
@@ -0,0 +1,19 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserslist" field in package.json
+    "postcss-import": {},
+    "autoprefixer": {},
+    "postcss-pxtorem": { // 此处为添加部分
+      rootValue: 750/16, //结果为：设计稿元素尺寸/16，算出比例，相当于1rem等于多少px
+      unitPrecision: 5, // 保留到几位小数
+      propList: ['*'],
+      selectorBlackList: ['font-no-rem'],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0,
+      exclude: /node_modules/i
+    }
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  html,body {
+    height: 100%;
+    overflow: hidden;
+  }
+  #app {
+    /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+    /*-webkit-font-smoothing: antialiased;*/
+    /*-moz-osx-font-smoothing: grayscale;*/
+    /*text-align: center;*/
+    /*color: #2c3e50;*/
+    /*margin-top: 60px;*/
+    height: 100%;
+    background: #EEEEEE;
+  }
+</style>
