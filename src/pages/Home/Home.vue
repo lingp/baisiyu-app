568bc2f88db80d83222117d192eb50d51a8502f6
lingp <carl.lin@huolala.cn>
2021-01-16 22:27:50 +0800

    上下拉动

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -10,13 +10,16 @@
     </Header>
     <div class="content">
       <div class="nav">
-        <cube-scroll-nav-bar :current="current" :labels="labels" :txts="txts" @change="changeHandler"/>
+        <cube-scroll-nav-bar :current="current" :labels="labels" :txts="txts" :items="items" @change="changeHandler"/>
       </div>
       <div class="list">
         <v-touch v-on:swipeleft="swiperleft"  v-on:swiperight="swiperright" class="wrapper">
           <cube-scroll
             ref="scroll"
             class="home-list-scroll"
+            :options="options"
+            @pulling-down="onPullingDown"
+            @pulling-up="onPullingUp"
           >
             <ul>
               <li class="item" v-for="item in articles">
@@ -35,109 +38,48 @@
                 </div>
                 <div class="item-content">
                   {{item.content}}
+                  <div class="item-content-footer clearfix">
+                    <div class="item-content-footer-area">
+                      <i class="iconfont icon-like"></i>
+                      <span class="area-text">{{item.likes}}</span>
+                    </div>
+                    <div class="item-content-footer-area">
+                      <i class="iconfont icon-comment"></i>
+                      <span class="area-text">{{item.comments}}</span>
+                    </div>
+                    <div class="item-content-footer-area">
+                      <i class="iconfont icon-view"></i>
+                      <span class="area-text">{{item.views}}</span>
+                    </div>
+                  </div>
                 </div>
                 <div class="item-footer"></div>
               </li>
-              <li class="item">
-                <div class="item-head clearfix">
-                  <div class="item-left">
-                  <span class="portrait">
-                     <img
-                       src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=246513285,2824505510&fm=26&gp=0.jpg"/>
-                  </span>
-                    <span class="nick">
-                    一切皆在不言中
-                  </span>
-                  </div>
-                  <div class="item-right">
-                    <i class="cubeic-close"></i>
-                  </div>
-                </div>
-                <div class="item-content">
-                  2 一等人是官倒，出了事情有人保；二等人是官倒，游山玩水享清福；三等人搞承包，吃喝嫖赌全报销；
-                  四等人干租赁，坑蒙拐骗带小姘；五等人是老板，买空卖空成老板；六等人是明星，挣的票子数不清；
-                  七等人是路霸，缺钱花了把车卡；八等人是律师，发财全靠打官司；九等人大盖帽，吃了原告吃被告；
-                  十等人是演员，扭扭屁股也赚钱；十一等人是工商，罚了款子兜里装；十二等人是画家，画完螃蟹画大虾；
-                  十三等人手术刀，割了肚子要红包；十四等人方向盘，上班下班都赚钱；十五等人搞个体，赚多赚少归自己；
-                  十六等人干宣传，隔三差五能解馋；十七等人是教员，山珍海味认不全；十八等人老百姓，学习雷锋干革命。
-                </div>
-                <div class="item-footer"></div>
-              </li>
-              <li class="item">
-                <div class="item-head clearfix">
-                  <div class="item-left">
-                  <span class="portrait">
-                     <img
-                       src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=246513285,2824505510&fm=26&gp=0.jpg"/>
-                  </span>
-                    <span class="nick">
-                    一切皆在不言中
-                  </span>
-                  </div>
-                  <div class="item-right">
-                    <i class="cubeic-close"></i>
-                  </div>
-                </div>
-                <div class="item-content">
-                  3 一等人是官倒，出了事情有人保；二等人是官倒，游山玩水享清福；三等人搞承包，吃喝嫖赌全报销；
-                  四等人干租赁，坑蒙拐骗带小姘；五等人是老板，买空卖空成老板；六等人是明星，挣的票子数不清；
-                  七等人是路霸，缺钱花了把车卡；八等人是律师，发财全靠打官司；九等人大盖帽，吃了原告吃被告；
-                  十等人是演员，扭扭屁股也赚钱；十一等人是工商，罚了款子兜里装；十二等人是画家，画完螃蟹画大虾；
-                  十三等人手术刀，割了肚子要红包；十四等人方向盘，上班下班都赚钱；十五等人搞个体，赚多赚少归自己；
-                  十六等人干宣传，隔三差五能解馋；十七等人是教员，山珍海味认不全；十八等人老百姓，学习雷锋干革命。
-                </div>
-                <div class="item-footer"></div>
-              </li>
-              <li class="item">
-                <div class="item-head clearfix">
-                  <div class="item-left">
-                  <span class="portrait">
-                     <img
-                       src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=246513285,2824505510&fm=26&gp=0.jpg"/>
-                  </span>
-                    <span class="nick">
-                    一切皆在不言中
-                  </span>
-                  </div>
-                  <div class="item-right">
-                    <i class="cubeic-close"></i>
-                  </div>
-                </div>
-                <div class="item-content">
-                  3 一等人是官倒，出了事情有人保；二等人是官倒，游山玩水享清福；三等人搞承包，吃喝嫖赌全报销；
-                  四等人干租赁，坑蒙拐骗带小姘；五等人是老板，买空卖空成老板；六等人是明星，挣的票子数不清；
-                  七等人是路霸，缺钱花了把车卡；八等人是律师，发财全靠打官司；九等人大盖帽，吃了原告吃被告；
-                  十等人是演员，扭扭屁股也赚钱；十一等人是工商，罚了款子兜里装；十二等人是画家，画完螃蟹画大虾；
-                  十三等人手术刀，割了肚子要红包；十四等人方向盘，上班下班都赚钱；十五等人搞个体，赚多赚少归自己；
-                  十六等人干宣传，隔三差五能解馋；十七等人是教员，山珍海味认不全；十八等人老百姓，学习雷锋干革命。
-                </div>
-                <div class="item-footer"></div>
-              </li>
-              <li class="item">
-                <div class="item-head clearfix">
-                  <div class="item-left">
-                  <span class="portrait">
-                     <img
-                       src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=246513285,2824505510&fm=26&gp=0.jpg"/>
-                  </span>
-                    <span class="nick">
-                    一切皆在不言中
-                  </span>
-                  </div>
-                  <div class="item-right">
-                    <i class="cubeic-close"></i>
-                  </div>
-                </div>
-                <div class="item-content">
-                  3 一等人是官倒，出了事情有人保；二等人是官倒，游山玩水享清福；三等人搞承包，吃喝嫖赌全报销；
-                  四等人干租赁，坑蒙拐骗带小姘；五等人是老板，买空卖空成老板；六等人是明星，挣的票子数不清；
-                  七等人是路霸，缺钱花了把车卡；八等人是律师，发财全靠打官司；九等人大盖帽，吃了原告吃被告；
-                  十等人是演员，扭扭屁股也赚钱；十一等人是工商，罚了款子兜里装；十二等人是画家，画完螃蟹画大虾；
-                  十三等人手术刀，割了肚子要红包；十四等人方向盘，上班下班都赚钱；十五等人搞个体，赚多赚少归自己；
-                  十六等人干宣传，隔三差五能解馋；十七等人是教员，山珍海味认不全；十八等人老百姓，学习雷锋干革命。
-                </div>
-                <div class="item-footer"></div>
-              </li>
+<!--              <li class="item">-->
+<!--                <div class="item-head clearfix">-->
+<!--                  <div class="item-left">-->
+<!--                  <span class="portrait">-->
+<!--                     <img-->
+<!--                       src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=246513285,2824505510&fm=26&gp=0.jpg"/>-->
+<!--                  </span>-->
+<!--                    <span class="nick">-->
+<!--                    一切皆在不言中-->
+<!--                  </span>-->
+<!--                  </div>-->
+<!--                  <div class="item-right">-->
+<!--                    <i class="cubeic-close"></i>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--                <div class="item-content">-->
+<!--                  3 一等人是官倒，出了事情有人保；二等人是官倒，游山玩水享清福；三等人搞承包，吃喝嫖赌全报销；-->
+<!--                  四等人干租赁，坑蒙拐骗带小姘；五等人是老板，买空卖空成老板；六等人是明星，挣的票子数不清；-->
+<!--                  七等人是路霸，缺钱花了把车卡；八等人是律师，发财全靠打官司；九等人大盖帽，吃了原告吃被告；-->
+<!--                  十等人是演员，扭扭屁股也赚钱；十一等人是工商，罚了款子兜里装；十二等人是画家，画完螃蟹画大虾；-->
+<!--                  十三等人手术刀，割了肚子要红包；十四等人方向盘，上班下班都赚钱；十五等人搞个体，赚多赚少归自己；-->
+<!--                  十六等人干宣传，隔三差五能解馋；十七等人是教员，山珍海味认不全；十八等人老百姓，学习雷锋干革命。-->
+<!--                </div>-->
+<!--                <div class="item-footer"></div>-->
+<!--              </li>-->
 
 
 
@@ -171,24 +113,99 @@
           '话题',
           '解忧'
         ],
-        items: [1, 2, 3, 4]
+        items: [1, 2, 3, 4],
+        pullDownRefresh: true,
+        pullDownRefreshThreshold: 60,
+        pullDownRefreshStop: 40,
+        pullDownRefreshTxt: 'Refresh success',
+        pullUpLoad: true,
+        pullUpLoadThreshold: 0,
+        pullUpLoadMoreTxt: 'Load more',
+        pullUpLoadNoMoreTxt: 'No more data',
+        customPullDown: false
       }
     },
     methods: {
       changeHandler(cur) {
         this.current = cur
-        // this.$router.replace('/search')
+        this.loadArticle()
       },
-      swiperleft: function () {  // 左划切换到goods页
-        alert('向左移动')
-        // this.$router.push({'path': '/'})
+      swiperleft: function () {  // 左划
+        // alert('向左移动')
+        let length = this.labels.length
+        let index = 0
+
+        for (index; index < length; index++) {
+          if (this.labels[index] === this.current) {
+            break
+          }
+        }
+        if (index >= length - 1) {
+          index = length - 1
+        } else {
+          index += 1
+        }
+
+        if (this.current !== this.labels[index]) {
+          this.current = this.labels[index]
+          // this.$router.push({'path': '/'})
+          this.loadArticle()
+        }
       },
-      swiperright: function () { // 右滑切换到detail页
-        alert('向右移动')
+      swiperright: function () { // 右滑
+        // alert('向右移动')
+        let length = this.labels.length
+        let index = 0
+
+        for (index; index < length; index++) {
+          if (this.labels[index] === this.current) {
+            break
+          }
+        }
+        if (index <= 0) {
+          index = 0
+        } else {
+          index -= 1
+        }
+        if (this.current !== this.labels[index]) {
+          this.current = this.labels[index]
+          this.loadArticle()
+        }
         // this.$router.push({'path': '/message'})
       },
+      onPullingDown: function () {
+        alert('pull-down')
+        // 模拟更新数据
+        setTimeout(() => {
+          if (Math.random() > 0.5) {
+            alert('pull-down')
+          } else {
+            // 如果没有新数据
+            this.$refs.scroll.forceUpdate()
+          }
+        }, 1000)
+      },
+      onPullingUp: function () {
+        alert('pull-up')
+        this.$refs.scroll.forceUpdate()
+      },
       loadArticle: function () { // 加载文章
+        // 加载文章列表
         let channelId = ''
+        switch (this.current) {
+          case 'recommend':
+            channelId = 1
+            break
+          case 'stars':
+            channelId = 2
+            break
+          case 'topic':
+            channelId = 3
+            break
+          case 'no_sorrow':
+            channelId = 4
+            break
+        }
         this.$store.dispatch('loadArticle', channelId)
       }
     },
@@ -203,7 +220,31 @@
       this.loadArticle()
     },
     computed: {
-      ...mapState(['articles'])
+      ...mapState(['articles']),
+      options() {
+        return {
+          pullDownRefresh: this.pullDownRefreshObj,
+          pullUpLoad: this.pullUpLoadObj,
+          scrollbar: true
+        }
+      },
+      pullDownRefreshObj: function() {
+        return this.pullDownRefresh ? {
+          threshold: parseInt(this.pullDownRefreshThreshold),
+          // Do not need to set stop value, but you can if you want
+          // stop: parseInt(this.pullDownRefreshStop),
+          txt: this.pullDownRefreshTxt
+        } : false
+      },
+      pullUpLoadObj: function() {
+        return this.pullUpLoad ? {
+          threshold: parseInt(this.pullUpLoadThreshold),
+          txt: {
+            more: this.pullUpLoadMoreTxt,
+            noMore: this.pullUpLoadNoMoreTxt
+          }
+        } : false
+      }
     }
   }
 </script>
@@ -314,6 +355,22 @@
           margin-left: 40px;
           /*white-space:normal;*/
           box-sizing: border-box;
+
+          .item-content-footer {
+            margin-top: 20px;
+            box-sizing: border-box;
+            width: 100%;
+            position: relative;
+            .item-content-footer-area {
+              float: left;
+              width: 33%;
+              text-align: center;
+              .area-text {
+                /*color: #E6E6E6;*/
+                font-size: 40px;
+              }
+            }
+          }
         }
 
         .item-footer {
